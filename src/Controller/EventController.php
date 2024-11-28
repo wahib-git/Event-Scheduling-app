6949b991d1ce8d4bcdf5d4b611311d291c0dc0b0
wahib-git <wahibbachoua95@gmail.com>
2024-11-28 13:56:29 +0100

    projet

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,6 +5,7 @@ use App\Entity\Event; // Assurez-vous d'importer l'entité Event
 use App\Form\EventType; // Assurez-vous d'importer le formulaire EventType
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface; // Importation nécessaire
@@ -21,7 +22,7 @@ class EventController extends AbstractController
             ['listeE'=>$listEvents]);
     }
     #[Route('/new', name: 'app_new')]
-    public function new(Request $request, EntityManagerInterface $em)
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
         $event = new Event();
         $form = $this->createForm(EventType::class, $event);
@@ -38,18 +39,31 @@ class EventController extends AbstractController
             'formE' => $form->createView()]);
     }
 
-    #[Route('/{id}', name: 'event_delete')]
-    public function delete(EntityManagerInterface $em, EventRepository $er, $id)
+    #[Route('/{id}',name: 'event_delete', requirements:["id"=>"\d+"])]
+    public function delete(EntityManagerInterface $em, EventRepository $er, $id): RedirectResponse
     {
         $event = $er->find($id);
         $em->remove($event);
         $em->flush();
-
         return $this->redirectToRoute('app_event');
     }
 
+    #[Route('/search', name: 'searchE')]
+    public function searchEvent(Request $request, EntityManagerInterface $em): Response
+    {
+        $nom = $request->request->get('eventName');
+
+        $q = $em->createQuery('select e from App\Entity\Event e where e.nom = :n');
+        $q->setParameter('n', $nom);
+
+        $events = $q->getResult();
+
+        return $this->render('event/searchEvent.html.twig', [
+            'listeE' => $events,
+        ]);
+    }
     #[Route('/{id}/edit', name: 'event_update')]
-    public function edit(Request $request, EntityManagerInterface $em, EventRepository $er, $id)
+    public function edit(Request $request, EntityManagerInterface $em, EventRepository $er, $id): Response
     {
         $event = $er->find($id);
 
